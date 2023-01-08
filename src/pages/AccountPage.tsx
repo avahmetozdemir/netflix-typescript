c434b3cd840a6f064481f759bf5f4b527f01ae66
avahmetozdemir <avamertozdemir@gmail.com>
2023-01-08 11:28:24 +0300

    21- add profiles to account page and some styles for other features

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -8,14 +8,14 @@ function AccountPage() {
     <div>
       <Navbar />
       <div className="w-[1000px] mx-auto">
-        <div className="flex border-b border-gray-500 space-x-3 items-center pb-5">
+        <div className="flex border-b border-gray-500 space-x-3 items-center p-2">
           <h1 className="text-4xl">Account</h1>
           <MdOutlineVideoLibrary color="red" />
           <h3 className=" text-gray-600">
             Member Since <span>March 2020</span>
           </h3>
         </div>
-        <div className="flex justify-between mt-2">
+        <div className="flex justify-between p-2">
           <div className=" flex flex-col flex-2/5 space-y-2">
             <h2 className="text-gray-500 text-lg">MEMBERSHIP & BILLING</h2>
             <div className="flex bg-gray-200 justify-center cursor-pointer w-[200px] ">
@@ -81,46 +81,91 @@ function AccountPage() {
             </div>
           </div>
         </div>
-        <div className="flex flex-row border-b border-gray-300">
+        <div className="flex flex-row justify-between border-b border-gray-300 p-2">
           <h1>PLAN DETAILS</h1>
           <h2>Premium ULTRA HD</h2>
+          <div></div>
           <p className="text-blue-600 hover:underline cursor-pointer">
             Change plan
           </p>
         </div>
-        <div className="flex flex-row border-b border-gray-300">
+        <div className="flex flex-row justify-between  border-b border-gray-300 p-2">
           <h1>SECURITY & PRIVACY</h1>
-          <p>
-            Control access to this account, view the most recently active
-            devices and more.
-          </p>
-          <p className="text-blue-600 hover:underline cursor-pointer">
-            Change plan
-          </p>
-          <div className="flex flex-col space-y-2">
-            <p className="text-blue-600 hover:underline cursor-pointer">
-              Manage access and devices
-            </p>
-            <p className="text-blue-600 hover:underline cursor-pointer">
-              Sign out of all devices
-            </p>
-            <p className="text-blue-600 hover:underline cursor-pointer">
-              Download your personal information
+          <div className="w-[300px]">
+            <p>
+              Control access to this account, view the most recently active
+              devices and more.
             </p>
           </div>
-        </div>
-        <div className="flex flex-row border-b border-gray-300">
-          <h1>PROFILE & PARENTAL CONTROLS</h1>
-          <div>
+          <div className="flex flex-row space-x-10">
             <div>
-              <div>
-                <img src="" alt="" />
-                <div>
-                  <p>Yüksel</p>
+              <p className="text-blue-600 hover:underline cursor-pointer">
+                Change plan
+              </p>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <p className="text-blue-600 hover:underline cursor-pointer">
+                Manage access and devices
+              </p>
+              <p className="text-blue-600 hover:underline cursor-pointer">
+                Sign out of all devices
+              </p>
+              <p className="text-blue-600 hover:underline cursor-pointer">
+                Download your personal information
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between border-b border-gray-300 p-2">
+          <h1 className="">PROFILE & PARENTAL CONTROLS</h1>
+          <div className="flex flex-col">
+            <div className="flex flex-row space-x-10 justify-between p-2 border-b border-gray-300">
+              <div className="flex flex-row space-x-5 items-center">
+                <img
+                  src="https://nextluxury.com/wp-content/uploads/Homer-Simpson-e1648441168355.jpg"
+                  alt=""
+                  className="w-12 h-12 rounded-xl"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Yüksel</p>
                   <p>All Maturity Ratings</p>
                 </div>
               </div>
-              <IoChevronDownOutline size={20} />
+              <div className="flex items-center">
+                <IoChevronDownOutline size={20} />
+              </div>
+            </div>
+            <div className="flex flex-row space-x-10 justify-between p-2 border-b border-gray-300">
+              <div className="flex flex-row space-x-5 items-center">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYqiJdk9eigV_PcJoKhZ95OZaLAkQ5YQ9z8bgIRkLp4EkIgZzHmb96nF3dbDqoNKPDv7k&usqp=CAU"
+                  alt=""
+                  className="w-10 h-12 rounded-xl"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Ahmet</p>
+                  <p>All Maturity Ratings</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <IoChevronDownOutline size={20} />
+              </div>
+            </div>
+            <div className="flex flex-row space-x-10 justify-between p-2 border-b border-gray-300">
+              <div className="flex flex-row space-x-5 items-center">
+                <img
+                  src="https://64.media.tumblr.com/b29d66d276cd8070d29ff53df060ee4d/tumblr_nrwp8j6yzZ1rxw2uko8_1280.jpg"
+                  alt=""
+                  className="w-12 h-12 rounded-xl"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Ebrar</p>
+                  <p>All Maturity Ratings</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <IoChevronDownOutline size={20} />
+              </div>
             </div>
           </div>
         </div>
